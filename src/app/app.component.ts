76f372b4262b6a40c46e5835e7eca629bec999c1
JP0805 <85284083+jpp0805@users.noreply.github.com>
2022-04-04 02:36:10 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDropList } from '@angular/cdk/drag-drop';
+
+// @ts-ignore
+import { Board } from './models/board.model';
+// @ts-ignore
+import { Column } from './models/column.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'untitled2';
+export class AppComponent implements OnInit{
+  name = 'Angular Material ' + VERSION.major + ' Kanban board';
+  public board: Board = new Board('Test Board', [
+    new Column('Ideas', '21', [
+      'Some random idea',
+      'This is another random idea'
+    ]),
+    new Column('Research', '32', [
+      'Lorem ipsum',
+      'foo'
+    ])
+  ]);
+
+  constructor(){}
+
+  public ngOnInit(): void {
+    console.log(this.board);
+  }
+
+  public dropGrid(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.board.columns, event.previousIndex, event.currentIndex);
+  }
+
+  public drop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 // @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';// @ts-ignore
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import {DragDropModule} from "@angular/cdk/drag-drop";
+// @ts-ignore
+import { ModelsComponent } from './models/models.component';
+import { BoardComponent } from './models/board/board.component';
+import { ColumnComponent } from './models/column/column.component';
+import { HelloComponent } from './hello/hello.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModelsComponent,
+    BoardComponent,
+    ColumnComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
-    DragDropModule
+    DragDropModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
