9880586a4b7ebab91450beca799d77504204790e
JP0805 <85284083+jpp0805@users.noreply.github.com>
2022-04-04 17:39:31 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,23 @@ import { NgModule } from '@angular/core';
 // @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';// @ts-ignore
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import {DragDropModule} from "@angular/cdk/drag-drop";
-// @ts-ignore
-import { ModelsComponent } from './models/models.component';
-import { BoardComponent } from './models/board/board.component';
-import { ColumnComponent } from './models/column/column.component';
-import { HelloComponent } from './hello/hello.component';
+
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AppRoutingModule} from "./app-routing/app-routing.module";
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ModelsComponent,
-    BoardComponent,
-    ColumnComponent,
-    HelloComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
     DragDropModule,
-    FormsModule,
+    // FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
